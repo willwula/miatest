9054ae8e34b8e066ef8d6615b7761b65d816cf90
willwula <wfh318@gmail.com>
2023-05-02 09:44:39 +0800

    feat:thirdPartyLogin

---
+++ resources/views/emails/orders/shipped.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div class="text-center">
+                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to <br>Book Collections</h1>
+                @guest()
+                <p class="mt-6 text-lg leading-8 text-gray-600">請登入享用所有服務</p>
+                <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <a href="/login" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</a>
+                    <a href="/register" class="text-sm font-semibold leading-6 text-gray-900">Sign up<span aria-hidden="true">→</span></a>
+                </div>
+                @endguest
+                @if(auth()->check())
+                    <p class="mt-6 text-lg leading-8 text-gray-600">Welcome, {{ auth()->user()->name }}</p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">開始旅程</a>
+                    </div>
+
+                @endif
+            </div>
+        </div>
+    </div>
+</x-layout>
---
+++ resources/views/emails/register-success.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>註冊成功通知</title>
+</head>
+<body>
+<h1>親愛的 {{ $user->name }}，</h1>
+
+<p>感謝您註冊我們的網站！您的帳戶已成功創建，現在可以登入了。</p>
+
+<p>謝謝，<br>susuuncle.shop</p>
+</body>
+</html>
+
+
